a17f9cc63846fbde5c026bbc7f54ca05b4931875
jamesdehan <jdmshomebuilder@gmail.com>
2026-05-22 20:51:49 -0300

    Criar lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+name: jd_home_builder
+description: JD Home Builder - Construa Seu Sonho | App Flutter com CRM, Funil de Vendas e Central de Imóveis
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: '>=3.0.0 <4.0.0'
+
+dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.6
+  url_launcher: ^6.2.5
+  image_picker: ^1.0.7
+  carousel_slider: ^4.2.1
+  intl: ^0.19.0
+  provider: ^6.1.1
+  shared_preferences: ^2.2.2
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^3.0.0
+
+flutter:
+  uses-material-design: true
